3949f15a4832fa594059d839bf2d3d775c034ecf
Taras Vytvytskyi <tarasvytvytskyy@gmail.com>
2023-12-03 16:58:01 +0200

    Lab5 AK-2

---
+++ Linux_kernel_modules_Lab5/hello1.c
@@ -0,0 +1,58 @@
+
+
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/printk.h>
+#include <linux/slab.h>
+#include <linux/ktime.h>
+#include <linux/list.h>
+#include <linux/types.h>
+#include "hello1.h"
+
+struct event_entry_t {
+	ktime_t time;
+	ktime_t time_after_print;
+	struct list_head next;
+};
+
+MODULE_AUTHOR("Taras Vytvytskyi");
+MODULE_DESCRIPTION("Linked list hello-printer API");
+MODULE_LICENSE("Dual BSD/GPL");
+
+static LIST_HEAD(events_list_head);
+#define HELLO1_DESIRED_FAULT 5
+
+int print_hello(unsigned int howmany)
+{
+	struct event_entry_t *event = NULL;
+	int i;
+
+	for (i = 0; i < howmany; i++) {
+		if (i == HELLO1_DESIRED_FAULT)
+			event = NULL;
+		else
+			event = kmalloc(sizeof(struct event_entry_t), GFP_KERNEL);
+		event->time = ktime_get();
+		INIT_LIST_HEAD(&event->next);
+		list_add_tail(&event->next, &events_list_head);
+		pr_info("Hello, world!\n");
+		event->time_after_print = ktime_get();
+	}
+
+	return 0;
+}
+EXPORT_SYMBOL(print_hello);
+
+void exit_hello(void)
+{
+	struct event_entry_t *md, *tmp;
+	int counter = 0;
+
+	list_for_each_entry_safe(md, tmp, &events_list_head, next) {
+		pr_info("Print #%d took %lld nanoseconds\n",
+			   counter++, md->time_after_print - md->time);
+		list_del(&md->next);
+		kfree(md);
+	}
+}
+EXPORT_SYMBOL(exit_hello);
---
+++ Linux_kernel_modules_Lab5/hello2.c
@@ -0,0 +1,36 @@
+
+
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/printk.h>
+#include <linux/slab.h>
+#include <linux/bug.h>
+#include "hello1.h"
+
+MODULE_AUTHOR("Taras Vytvytskyi");
+MODULE_DESCRIPTION("Hello, world utilising linked list API");
+MODULE_LICENSE("Dual BSD/GPL");
+
+static unsigned int howmany = 1;
+module_param(howmany, uint, 0444);
+MODULE_PARM_DESC(howmany, "Amount of loop runs");
+
+
+static int __init hello_init(void)
+{
+	BUG_ON(howmany > 10);
+	if ((howmany == 0) || ((howmany >= 5) && (howmany <= 10)))
+		pr_warn("Warning: input parameter is within specified range\n");
+
+	print_hello(howmany);
+
+	return 0;
+}
+
+static void __exit hello_exit(void)
+{
+	exit_hello();
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
